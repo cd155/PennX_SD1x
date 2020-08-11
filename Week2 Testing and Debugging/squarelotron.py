1fcd0b53b41a12adf2bcd204067754e15a1b61d1
Don <cd155@163.com>
2020-08-11 22:39:04 +0800

    finish upside_down_flip and main_diagonal_flip

--- Week2 Testing and Debugging/squarelotron.py
+++ Week2 Testing and Debugging/squarelotron.py
@@ -1,14 +1,85 @@
 class Squarelotron:
     def __init__(self, size=1):
-        self.squarelotron = []
         self.size = size
         self.number_of_rings = size/2 + size%2
 
-    def upside_down_flip(ring):
-        pass
+        self.squarelotron = []
+        for i in range(size):
+            arrays = []
+            for j in range (1, size + 1):
+                 arrays.append(i*size + j)
+            self.squarelotron.append(arrays)
 
-    def main_diagonal_flip(ring):
-        pass
+    def upside_down_flip(self, ring):
+        # check ring size
+        if ring > self.number_of_rings or ring < 1:
+            print("Ring input must with 1<=ring<=" + self.number_of_rings)
+            return
 
-    def rotate_right(number_of_turns):
-        pass
+        # copy a matrix to edit
+        matrix = self.squarelotron.copy()
+
+        # set bound
+        top_bound = 0
+        bottom_bound = self.size
+        out_number_of_rings = ring -1
+        if out_number_of_rings > 0:
+            top_bound = out_number_of_rings
+            bottom_bound = self.size - out_number_of_rings
+
+        half_row = (top_bound + bottom_bound)/2
+        for i in range(top_bound, int(half_row)):
+            for j in range(top_bound, bottom_bound):
+                if i == top_bound:
+                    temp = matrix[i][j]
+                    matrix[i][j] = matrix[self.size - 1 - i][j]
+                    matrix[self.size - 1 - i][j] = temp
+                else:
+                    if j == top_bound or j == bottom_bound - 1:
+                        temp = matrix[i][j]
+                        matrix[i][j] = matrix[self.size - 1 - i][j]
+                        matrix[self.size - 1 - i][j] = temp
+        return matrix
+
+    def main_diagonal_flip(self, ring):
+        # check ring size
+        if ring > self.number_of_rings or ring < 1:
+            print("Ring input must with 1<=ring<=" + self.number_of_rings)
+            return
+
+        # copy a matrix to edit
+        matrix = self.squarelotron.copy()
+
+        # set bound
+        top_bound = 0
+        bottom_bound = self.size
+        out_number_of_rings = ring -1
+        if out_number_of_rings > 0:
+            top_bound = out_number_of_rings
+            bottom_bound = self.size - out_number_of_rings
+
+        for i in range(top_bound, bottom_bound):
+            if i == bottom_bound -1:
+                for j in range(top_bound, bottom_bound):
+                    temp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = temp
+            else:
+                temp = matrix[i][out_number_of_rings]
+                matrix[i][out_number_of_rings] = matrix[out_number_of_rings][i]
+                matrix[out_number_of_rings][i] = temp
+        return matrix
+
+    def rotate_right(self, number_of_turns):
+
+        def clock_wise(self, number_of_turns):
+            pass
+
+        def counter_clock_wise(self, number_of_turns):
+            pass
+
+        def clock_wise_conner_adjust(self, number_of_turns):
+            pass
+
+        def counter_clock_wise_conner_adjust(self, number_of_turns):
+            pass
--- Week2 Testing and Debugging/test_squarelotron.py
+++ Week2 Testing and Debugging/test_squarelotron.py
@@ -8,16 +8,31 @@ class TestSquarelotron(unittest.TestCase):
     def tearDown(self):
         self.sample = None
 
-    def test_upside_down_flip(self):
-        self.assertEqual(self.sample.squarelotron, [])
+    def test_matrix_generate(self):
+        self.assertEqual(self.sample.squarelotron, [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
 
-    def test_main_diagonal_flip(self):
-        self.assertEqual(self.sample.squarelotron, [])
+    def test_upside_down_flip_ring_one(self):
+        self.assertEqual(self.sample.upside_down_flip(1), [[21,22,23,24,25], [16,7,8,9,20], [11,12,13,14,15], [6,17,18,19,10], [1,2,3,4,5]])
+
+    def test_upside_down_flip_ring_two(self):
+        self.assertEqual(self.sample.upside_down_flip(2), [[1,2,3,4,5], [6,17,18,19,10], [11,12,13,14,15], [16,7,8,9,20], [21,22,23,24,25]])
+
+    def test_upside_down_flip_ring_three(self):
+        self.assertEqual(self.sample.upside_down_flip(3), [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
+
+    def test_main_diagonal_flip_ring_one(self):
+        self.assertEqual(self.sample.main_diagonal_flip(1), [[1,6,11,16,21], [2,7,8,9,22], [3,12,13,14,23], [4,17,18,19,24], [5,10,15,20,25]])
+
+    def test_main_diagonal_flip_ring_two(self):
+        self.assertEqual(self.sample.main_diagonal_flip(2), [[1,2,3,4,5], [6,7,12,17,10], [11,8,13,18,15], [16,9,14,19,20], [21,22,23,24,25]])
+
+    def test_main_diagonal_flip_ring_three(self):
+        self.assertEqual(self.sample.main_diagonal_flip(3), [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
     
-    def test_rotate_right(self):
-        for number_of_turns in [-1, 0, 1]:
-            self.sample.rotate_right(number_of_turns)
-            self.assertEqual(self.sample.squarelotron, [])
+    # def test_rotate_right(self):
+    #     for number_of_turns in [-1, 0, 1]:
+    #         self.sample.rotate_right(number_of_turns)
+    #         self.assertEqual(self.sample.squarelotron, [])
 
 if __name__ == '__main__':
     unittest.main()
