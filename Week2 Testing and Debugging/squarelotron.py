5b3c69267608cdfcacaedcfe1b5796efabcfa343
Don <cd155@163.com>
2020-08-12 21:43:05 +0800

    finish rotate_right function and unit test

--- Week2 Testing and Debugging/squarelotron.py
+++ Week2 Testing and Debugging/squarelotron.py
@@ -72,14 +72,115 @@ class Squarelotron:
 
     def rotate_right(self, number_of_turns):
 
-        def clock_wise(self, number_of_turns):
-            pass
+        def clock_wise(number_of_turns):
+            for _ in range(number_of_turns):
+                top_bound = 0
+                bottom_bound = self.size
+                for i in range(int(self.size/2)):
+                    # collect conners
+                    conners = []
+                    conners.append(self.squarelotron[top_bound][top_bound])
+                    conners.append(self.squarelotron[top_bound][bottom_bound-1])
+                    conners.append(self.squarelotron[bottom_bound-1][top_bound])
+                    conners.append(self.squarelotron[bottom_bound-1][bottom_bound-1])
 
-        def counter_clock_wise(self, number_of_turns):
-            pass
+                    temp1 = []
+                    temp2 = []
 
-        def clock_wise_conner_adjust(self, number_of_turns):
-            pass
+                    # Example: 5 by 5 matrix
+                    # order: row 0 > colum 4> row 4 > colum 0
+                    # part1: row 0
+                    # part2: colum 4
+                    # part3: row 4
+                    # part4: colum 0
+                    # replace part2 with part1
+                    for j in range(top_bound, bottom_bound):
+                        temp1.append(self.squarelotron[j][self.size - 1 - i])
+                        self.squarelotron[j][self.size - 1 - i] = self.squarelotron[i][j]
 
-        def counter_clock_wise_conner_adjust(self, number_of_turns):
-            pass
+                    # replace part3 with part2
+                    temp1.reverse()
+                    for j in range(top_bound, bottom_bound):
+                        temp2.append(self.squarelotron[self.size - 1 - i][j])
+                        self.squarelotron[self.size - 1 - i][j] = temp1.copy()[j - top_bound]
+                    temp1.clear()
+
+                    # replace part4 with part3
+                    for j in range(top_bound, bottom_bound):
+                        temp1.append(self.squarelotron[j][i])
+                        self.squarelotron[j][i] = temp2[j - top_bound]
+
+                    # replace part1 with part4
+                    temp1.reverse()
+                    for j in range(top_bound, bottom_bound):
+                        self.squarelotron[i][j] = temp1[j - top_bound]
+                    
+                    # adjust conners
+                    self.squarelotron[top_bound][top_bound] = conners[2]
+                    self.squarelotron[top_bound][bottom_bound - 1] = conners[0]
+                    self.squarelotron[bottom_bound - 1][top_bound] = conners[3]
+                    self.squarelotron[bottom_bound - 1][bottom_bound - 1] = conners[1]
+
+                    # adjust top bottom bound
+                    top_bound += 1
+                    bottom_bound -= 1
+
+        def counter_clock_wise(number_of_turns):
+            for _ in range(abs(number_of_turns)):
+                top_bound = 0
+                bottom_bound = self.size
+                for i in range(int(self.size/2)):
+                    # collect conners
+                    conners = []
+                    conners.append(self.squarelotron[top_bound][top_bound])
+                    conners.append(self.squarelotron[top_bound][bottom_bound-1])
+                    conners.append(self.squarelotron[bottom_bound-1][top_bound])
+                    conners.append(self.squarelotron[bottom_bound-1][bottom_bound-1])
+
+                    temp1 = []
+                    temp2 = []
+
+                    # Example: 5 by 5 matrix
+                    # order: row 0 > colum 0> row 4 > colum 4
+                    # part1: row 0
+                    # part2: colum 4
+                    # part3: row 4
+                    # part4: colum 0
+                    # replace part4 with part1
+                    for j in range(top_bound, bottom_bound):
+                        temp1.append(self.squarelotron[j][i])
+                        self.squarelotron[j][i] = self.squarelotron[i][bottom_bound - 1 - (j - top_bound)]
+
+                    # replace part3 with part4
+                    for j in range(top_bound, bottom_bound):
+                        temp2.append(self.squarelotron[self.size - 1 - i][j])
+                        self.squarelotron[self.size - 1 - i][j] = temp1.copy()[j - top_bound]
+                    temp1.clear()
+
+                    # replace part2 with part3
+                    temp2.reverse()
+                    for j in range(top_bound, bottom_bound):
+                        temp1.append(self.squarelotron[j][self.size - 1 - i])
+                        self.squarelotron[j][self.size - 1 - i] = temp2[j - top_bound]
+
+                    # replace part1 with part2
+                    for j in range(top_bound, bottom_bound):
+                        self.squarelotron[i][j] = temp1[j - top_bound]
+                    
+                    # adjust conners
+                    self.squarelotron[top_bound][top_bound] = conners[1]
+                    self.squarelotron[top_bound][bottom_bound - 1] = conners[3]
+                    self.squarelotron[bottom_bound - 1][top_bound] = conners[0]
+                    self.squarelotron[bottom_bound - 1][bottom_bound - 1] = conners[2]
+
+                    # adjust top bottom bound
+                    top_bound += 1
+                    bottom_bound -= 1
+
+        if self.size == 1:
+            return self.squarelotron
+
+        if number_of_turns > 0:
+            clock_wise(number_of_turns)
+        else:
+            counter_clock_wise(number_of_turns)
--- Week2 Testing and Debugging/test_squarelotron.py
+++ Week2 Testing and Debugging/test_squarelotron.py
@@ -29,10 +29,17 @@ class TestSquarelotron(unittest.TestCase):
     def test_main_diagonal_flip_ring_three(self):
         self.assertEqual(self.sample.main_diagonal_flip(3), [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
     
-    # def test_rotate_right(self):
-    #     for number_of_turns in [-1, 0, 1]:
-    #         self.sample.rotate_right(number_of_turns)
-    #         self.assertEqual(self.sample.squarelotron, [])
+    def test_rotate_right_clockwise(self):
+        self.sample.rotate_right(1)
+        self.assertEqual(self.sample.squarelotron, [[21,16,11,6,1], [22,17,12,7,2], [23,18,13,8,3], [24,19,14,9,4], [25,20,15,10,5]])
+
+    def test_rotate_right_zero(self):
+        self.sample.rotate_right(0)
+        self.assertEqual(self.sample.squarelotron, [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
+
+    def test_rotate_right_counter_clockwise(self):
+        self.sample.rotate_right(-1)
+        self.assertEqual(self.sample.squarelotron, [[5,10,15,20,25], [4,9,14,19,24], [3,8,13,18,23], [2,7,12,17,22], [1,6,11,16,21]])
 
 if __name__ == '__main__':
     unittest.main()
