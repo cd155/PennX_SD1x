e301d098058372364d8dcfc95d4743149c0f0c4d
Don <cd155@163.com>
2020-08-17 23:32:30 +0800

    add new method and change constructor

--- Week3 Collections and Object Oriented Design/actor.py
+++ Week3 Collections and Object Oriented Design/actor.py
@@ -1,14 +1,17 @@
 class Actor:
 
-    def __init__(self, name, movies=[]) -> None:
+    def __init__(self, name):
         self.__name = name
-        self.__movies = movies
+        self.__movies = []
 
     def get_actor_name(self):
         return self.__name
-
-    def set_actor_name(self, new_name):
-        self.__name = new_name
     
     def get_movies(self):
         return self.__movies
+
+    def set_actor_name(self, new_name):
+        self.__name = new_name
+
+    def set_movies(self, new_movies):
+        self.__movies = new_movies
