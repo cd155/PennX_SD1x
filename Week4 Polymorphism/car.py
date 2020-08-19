3d9000f1050595599adcfa2dc2fcd9f88df15ae5
Don <cd155@163.com>
2020-08-19 23:47:20 +0800

    add car class

---
+++ Week4 Polymorphism/car.py
@@ -0,0 +1,25 @@
+class Car:
+
+    def __init__(self, owner_name, license_num, registration_num):
+        self.__owner_name = owner_name
+        self.__license_num = license_num
+        self.__registration_num = registration_num
+        self.__parking_location = 0
+    
+    def get_owner_name(self):
+        return self.__owner_name
+    
+    def set_owner_name(self, new_name):
+        self.__owner_name = new_name
+    
+    def get_owner_license_num(self):
+        return self.__license_num
+    
+    def get_owner_registration_num(self):
+        return self.__registration_num
+    
+    def get_parking_location(self):
+        return self.__parking_location
+    
+    def set_parking_location(self, new_location):
+        self.__parking_location = new_location
