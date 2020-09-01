24dd9600e6463df4a47ffb1c53c36a8c6985488f
Don <cd155@163.com>
2020-09-02 01:04:19 +0800

    override in emptysea class

--- Week4 Polymorphism/empty_sea.py
+++ Week4 Polymorphism/empty_sea.py
@@ -1,5 +1,18 @@
 from ship import Ship
 
 class EmptySea(Ship):
-    size = 0
-    pass
+
+    def __init__(self):
+        self.length = 1
+
+    def shoot_at(self, row, column):
+        return False
+
+    def is_sunk():
+        return False
+
+    def __str__(self):
+        return "e"
+
+    def get_ship_type(self):
+        return "empty"
