fab29adf9fe2773077f79a66c27f57a57ce3306f
Don <cd155@163.com>
2020-09-02 01:04:52 +0800

    implement in abstract class ship

--- Week4 Polymorphism/ship.py
+++ Week4 Polymorphism/ship.py
@@ -1,9 +1,50 @@
-from abc import ABC, abstractmethod 
+from abc import ABC, abstractmethod, abstractproperty
 
 class Ship(ABC):
-    def __init__(self):
+    @property
+    @abstractmethod
+    def bow_row(self):
+        pass
+
+    @property
+    @abstractmethod
+    def bow_column(self):
+        pass
+
+    @property
+    @abstractmethod    
+    def length(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def horizontal(self):
+        pass
+
+    @property
+    @abstractmethod
+    def hit(self):
         pass
 
     @abstractmethod
-    def abstractmethod():
-        pass
+    def get_ship_type(self):
+        raise NotImplementedError
+
+    def ok_to_place_ship_at(self, row, column, horizontal, ocean):
+        raise NotImplementedError
+
+    def place_ship_at(self, row, column, horizontal, ocean):
+        raise NotImplementedError
+
+    def shoot_at(self, row, column):
+        raise NotImplementedError
+
+    def is_sunk(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        if self.is_sunk():
+            return "x"
+        else:
+            return "S"
+            
